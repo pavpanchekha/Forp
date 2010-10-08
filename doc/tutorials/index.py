ac2e493d0a697af20969a362c884cc86628a757e
Pavel Panchekha <pavpanchekha@gmail.com>
2010-10-08 16:13:51 -0400

    Added documentation files

---
+++ doc/tutorials/index.py
@@ -0,0 +1,39 @@
+Forp Tutorials
+==============
+
+The tutorials here are for anyone who isn't familiar with Forp
+programming, whether in the general or for a specific application.
+They're broken into three sections: for non-programmers, for
+programmers, and for specific applications.
+
+For Non-Programmers
+-------------------
+
+.. toctree::
+    :maxdepth: 2
+    
+    beginning-forp
+
+For Programmers
+---------------
+
+.. toctree::
+    :maxdepth: 2
+
+    how-forp-different
+    forp-design
+
+    for-c-programmers
+    for-java-programmers
+    for-python-programmers
+    for-lisp-programmers
+
+Specific Applications
+---------------------
+
+.. toctree::
+    :maxdepth: 2
+
+    mathematics
+    web-applications
+
